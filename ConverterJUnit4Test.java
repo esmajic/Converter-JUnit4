b2da3364b2e758f83a6f6ea08a2561596f782a93
unknown <esmajic@yahoo.com>
2019-05-27 16:44:31 +0200

    created test method for kgsToPounds method

--- ConverterJUnit4Test.java
+++ ConverterJUnit4Test.java
@@ -11,18 +11,33 @@ public class ConverterJUnit4Test {
 	@Before
 	public void setUp() {
 		convert = new ConverterJUnit4();
-		System.out.println("BEFORE");
+		System.out.println("poundsToKgs");
 	}
 
 	@Test
 	public void shouldConvertPoundsToKilograms() {
 		double result = convert.poundsToKgs(1.0);
-		assertEquals(0.4535923, result, 0.01);
+		assertEquals(0.4535923, result, 0.001);
 	}
 
 	@After
 	public void tearDown() {
-		System.out.println("AFTER");
+	}
+	
+	@Before
+	public void setUp1() {
+		convert = new ConverterJUnit4();
+		System.out.println("kgsToPounds");
+	}
+
+	@Test
+	public void shouldConvertKgsToPounds() {
+		double result = convert.kgsToPounds(1.0);
+		assertEquals(0.39370, result, 0.001);
+	}
+
+	@After
+	public void tearDown1() {
 	}
 
 }
